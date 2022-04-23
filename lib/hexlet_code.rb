5ef732b591aafab4da47804220f1240f53f04fd5
Trufanov Nikita <trufanovwork@gmail.com>
2022-04-23 23:47:13 +0300

    first

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
+require 'nokogiri'
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+  class Tag
+    def self.build(first, **kwargs, &block)
+      kwargs.each_with_object([]) do |(k, v), array|
+        @smth = array << " #{k}=#{v}"
+      end
+      "<#{first}>#{yield}</#{first}>" if block_given?
+      "<#{([first] << @smth).join}>"
+    end
+  end
 end
+p HexletCode::Tag.build('label', for: 'email') { 'Email' }
