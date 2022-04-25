6ea8fe66443b39a73f4094821ca900a0a73f34c4
Trufanov Nikita <trufanovwork@gmail.com>
2022-04-25 20:12:24 +0300

    add git actions

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -11,8 +11,8 @@ class TestHexletCode < Minitest::Test
     tag_1 = HexletCode::Tag.build('input', type: 'submit', value: 'Save')
     tag_2 = HexletCode::Tag.build('label', for: 'email') { 'Email' }
     tag_3 = HexletCode::Tag.build('div'){}
-    assert_equal tag_1, ("<input type='submit' value='Save'>")
+    assert_equal tag_1, "<input type='submit' value='Save'>"
     assert_equal tag_2, "<label for='email'>Email</label>"
-    assert (tag_3 == "<div></div>")
+    assert(tag_3 == "<div></div>")
   end
 end
