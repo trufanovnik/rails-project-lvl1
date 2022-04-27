db4d5a8a75a688f580ec4ad8a75415377c5b45eb
Trufanov Nikita <trufanovwork@gmail.com>
2022-04-27 13:34:10 +0300

    push with tag class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
-require_relative "tag"
+require_relative "hexlet_code/tag"
 
 module HexletCode
   class Error < StandardError; end
   Tag.build(name, **kwargs, &block)
 end
-p HexletCode::Tag.build('input', type: 'submit', value: 'Save')
+
+puts HexletCode::Tag.build('input', type: 'submit', value: 'Save')
---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+class Tag
+  def self.build(name, **kwargs, &block)
+    tag = []
+    tag << tag_open(name, **kwargs) 
+    tag << tag_block(&block) if block_given?
+    tag << tag_close(name) if block_given?
+    tag.join
+  end
+
+  def self.tag_open(name, **kwargs)
+    kwargs.each_with_object([]) do |(k, v), array|
+      @str = array << " #{k}='#{v}'"
+    end
+    "<#{name}#{@str.join}>"
+  end
+  def self.tag_close(name)
+    "</#{name}>"
+  end
+  def self.tag_block(&block)
+    ">#{block.call}<" if block_given?
+  end
+end
+
+puts Tag.build('input', type: 'submit', value: 'Save')
