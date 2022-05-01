9d532af8a0be9159a3f2719d753af7986c995c25
Trufanov Nikita <trufanovwork@gmail.com>
2022-05-01 21:02:48 +0300

    add rows, cols

--- lib/hexlet_code/content_make.rb
+++ lib/hexlet_code/content_make.rb
@@ -12,16 +12,26 @@ module HexletCode
       label(field_name)
       @result << Tag.build("input", name: field_name.to_s, type: "text", value: field_value.to_s)
       @result << "\n"
+      add_as(field_name) if as
     end
 
     def label(field_name)
       field_value = @model.send(field_name)
-      @result << Tag.build("label", for: field_name.to_s) { field_value.to_s.capitalize }
+      @result << Tag.build("label", for: field_name.to_s) do
+        field_value.to_s.capitalize
+      end
       @result << "\n"
     end
 
     def submit(default = "Save")
       @result << Tag.build("input", name: "commit", type: "submit", value: default)
+    end
+
+    def add_as(field_name)
+      field_value = @model.send(field_name)
+      @result << Tag.build("textarea", cols: "20", rows: "40", name: field_name.to_s) do
+        field_value.to_s
+      end
     end
     @result
   end
